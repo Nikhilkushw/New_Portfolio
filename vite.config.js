91bed42292f8a4ad24f1b356de2a880d1ab43902
Nikhil Kushwah <nikhilkush037@gmail.com>
2025-04-04 17:47:43 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/New_Portfolio/', // 👈 Add this line
   plugins: [react()],
+  base: '/New_portfolio/', // ✅ This must match your repo name
 })
