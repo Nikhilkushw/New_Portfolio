602135ea170286215655e7ebdcea5c3a072b454a
Nikhil Kushwah <nikhilkush037@gmail.com>
2025-04-04 16:35:13 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Navbar from './Navbar/navbar'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar/navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
